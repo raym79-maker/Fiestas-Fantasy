67c0cfd9139f3407323c9cab8a0ebc91f758db62
raym79-maker <raym79@gmail.com>
2026-08-20 16:29:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,18 @@ app.get('/fiestas-familiares', (req, res) => {
   res.sendFile(path.join(__dirname, 'fiestas-familiares', 'index.html'));
 });
 
+app.get('/blog/ideas-convivio-empresa-bajio', (req, res) => {
+  res.sendFile(path.join(__dirname, 'blog', 'ideas-convivio-empresa-bajio', 'index.html'));
+});
+
+app.get('/blog/que-es-casino-entretenimiento', (req, res) => {
+  res.sendFile(path.join(__dirname, 'blog', 'que-es-casino-entretenimiento', 'index.html'));
+});
+
+app.get('/blog/quinceañera-diferente-querétaro', (req, res) => {
+  res.sendFile(path.join(__dirname, 'blog', 'quinceañera-diferente-querétaro', 'index.html'));
+});
+
 // Para cualquier otra ruta no encontrada, servir el index principal
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
